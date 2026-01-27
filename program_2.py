3adba9a4547dc7a1041b7bded9ba668147aa27c1
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-01-27 15:55:33 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,12 +1,14 @@
-  #Author: Sam Gaines
-  #Date: 1/15/2026
-  #Title: Modified Hello World Part II
-  def proverbs_22_29():
-    print("Do you see someone skilled in their work?")    
-    print()    
-    print("They will serve before kings;")    
-    print()    
-    print("they will not serve before officials of low rank.")    
-    print()    
-    print("(Proverbs 29:22)")    
- proverbs_22_29()
+# Author: Sam Gaines
+# Date: 1/15/2026
+# Title: Modified Hello World Part II
+
+def proverbs_22_29():
+    print("Do you see someone skilled in their work?")
+    print()
+    print("They will serve before kings;")
+    print()
+    print("They will not serve before officials of low rank.")
+    print()
+    print("(Proverbs 29:22)")
+
+proverbs_22_29()
