7f8a0e3f47dca63d5337e627c900cda064c8596b
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-01-27 15:35:30 -0600

    Update program_1.py
    correct version

--- program_1.py
+++ program_1.py
@@ -1,5 +1,6 @@
-#Author: Sam Gaines
-#Date: 1/14/2026 
-#Title: Modified Hello World Program 
-def Hello_World():
-    print('Hello World')
+ #Author: Sam Gaines
+ #Date: 1/14/2026 
+ #Title: Modified Hello World Program 
+ def Hello_World():
+    print('Hello World') 
+Hello_World()
