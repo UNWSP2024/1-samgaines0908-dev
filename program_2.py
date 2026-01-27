ff2e59a251aafb33aa4aa620ed337ef2f5aae9da
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-01-27 15:33:05 -0600

    Update program_2.py
    correct version

--- program_2.py
+++ program_2.py
@@ -1,13 +1,12 @@
-def proverbs_22_29():
-    # Write your Proverbs 22:29 code below
-    #Author: Sam Gaines
-#Date: 1/15/2026
-#Title: Modified Hello World Part II
-print()
-print("Do you see someone skilled in their work")
-print()
-print(      "They will serve before kings;")
-print()
-print("they will not serve before officials of low rank")
-print()
-print("        (Proverbs 29:22)")   
+ #Author: Sam Gaines
+ #Date: 1/15/2026
+ #Title: Modified Hello World Part II
+ def proverbs_22_29():
+    print("Do you see someone skilled in their work?")
+    print()
+    print("They will serve before kings;")
+    print()
+    print("they will not serve before officials of low rank.")
+    print()
+    print("(Proverbs 29:22)")
+proverbs_22_29()
